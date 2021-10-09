231bea0dc8c4c818d976a5da37c477571e801836
dschwoerer <dschwoerer@users.noreply.github.com>
2021-10-09 21:20:26 +0000

    [skip ci] Apply black changes

--- docs/examples/setup_plt.py
+++ docs/examples/setup_plt.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 
 
 if "DOCUTILSCONFIG" in os.environ:
-    get_ipython().run_line_magic('matplotlib', 'inline')
+    get_ipython().run_line_magic("matplotlib", "inline")
     figsize = 10.0
 else:
-    get_ipython().run_line_magic('matplotlib', 'notebook')
+    get_ipython().run_line_magic("matplotlib", "notebook")
     figsize = 6.0
-plt.rcParams['figure.figsize'] = [figsize, figsize]
+plt.rcParams["figure.figsize"] = [figsize, figsize]
