b99fd04a76186ff3e6198303b0ee921bc44b688f
David Bold <dave@ipp.mpg.de>
2024-06-03 16:04:45 +0200

    Expose ignore_broken in CLI

--- xemc3/cli/to_netcdf.py
+++ xemc3/cli/to_netcdf.py
@@ -8,11 +8,11 @@ from ..core.load import load_all
 from ._common import commonparser, iter_dir
 
 
-def to_netcdf(read: str, name: str, quiet: bool = True) -> None:
+def to_netcdf(read: str, name: str, quiet: bool, ignore_broken: bool) -> None:
     if not quiet:
         print(f"Loading {read} ...", end="")
         sys.stdout.flush()
-    ds = load_all(read)
+    ds = load_all(read, ignore_broken=ignore_broken)
     if not quiet:
         print(" writing ...", end="")
         sys.stdout.flush()
@@ -22,17 +22,23 @@ def to_netcdf(read: str, name: str, quiet: bool = True) -> None:
 
 
 def parser() -> ArgumentParser:
-    return commonparser(
+    cp = commonparser(
         "Load the data from EMC3 simulations and store as netcdf file. "
         "The data is written for each simulation to a netcdf file."
     )
+    cp.add_argument(
+        "--ignore-broken",
+        action="store_true",
+        help="Ignore if mapped files are incomplete",
+    )
+    return cp
 
 
 def main() -> None:
     args = parser().parse_args()
 
     for d, n in iter_dir(args):
-        to_netcdf(d, n, args.quiet)
+        to_netcdf(d, n, args.quiet, ignore_broken=args.ignore_broken)
 
 
 if __name__ == "__main__":
