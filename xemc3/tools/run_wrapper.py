843a387c3e36bc864ad72d77992dd307b9f3a9cc
dschwoerer <dschwoerer@users.noreply.github.com>
2025-02-05 12:25:16 +0000

    Apply black changes

--- xemc3/tools/run_wrapper.py
+++ xemc3/tools/run_wrapper.py
@@ -1,19 +1,19 @@
 """Collection of functions which can be used to make a BOUT++ run
 
-    This file is part of boututils.
+This file is part of boututils.
 
-    boututils is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+boututils is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    boututils is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
+boututils is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
 
-    You should have received a copy of the GNU Lesser General Public License
-    along with boututils.  If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Lesser General Public License
+along with boututils.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import os
