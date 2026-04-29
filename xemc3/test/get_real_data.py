c98ab359438d72b7aaa22720d26c2d238c6e1e3d
David Bold <dave@ipp.mpg.de>
2026-04-29 09:06:00 +0200

    Move function to own file

---
+++ xemc3/test/get_real_data.py
@@ -0,0 +1,23 @@
+import os
+
+def get_data(force=False):
+    basedir = "./example-data/"
+    if not os.path.isdir(basedir) and not force:
+        import pytest
+        pytest.skip("create {basedir} to enable testing on real data")
+        return
+    if not os.path.isdir(basedir + ".git"):
+        os.system(
+            f"git clone https://oauth2:glpat-gS1qiEX7Ncoys5xH3CfS@gitlab.mpcdf.mpg.de/dave/xemc3-data/ {basedir} --depth 1"
+        )
+    else:
+        os.system(f"cd {basedir}; git fetch origin main --depth 1")
+        os.system(f"cd {basedir}; git checkout origin main")
+    return basedir + "emc3_example"
+
+
+if __name__ == "__main__":
+    import sys
+
+    get_data(force=True)
+    sys.exit(0)
