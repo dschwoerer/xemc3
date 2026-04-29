7ec9105296e3589c7b0960e3bbd9e25d535aa914
dschwoerer <dschwoerer@users.noreply.github.com>
2026-04-29 06:45:17 +0000

    Apply black changes

--- docs/cli.rst.in.py
+++ docs/cli.rst.in.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
 
-print(
-    """CLI reference
+print("""CLI reference
 =============
 
-"""
-)
+""")
 
 path = __file__.split("/")
 if len(path) < 2:
@@ -29,12 +27,10 @@ with open("/".join(path)) as f:
 for l in found:
     name, ref = [x.strip() for x in l.split("=")]
     ref = ref.split(":")[0]
-    print(
-        f"""
+    print(f"""
 
 .. sphinx_argparse_cli::
    :module: {ref}
    :func: parser
    :prog: {name}
-"""
-    )
+""")
